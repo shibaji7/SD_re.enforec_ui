c205e0c28a76efca614423458aa459e4114467c9
shibaji7 <shibaji7@vt.edu>
2018-09-16 16:23:22 -0400

    Basic features with Django + Bootstrap3

---
+++ reenforce_ui_b/ui/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+
+from . import views
+#from __future__ import unicode_literals
+
+from django.conf.urls import url
+
+from .views import (
+        HomePageView,
+        FormHorizontalView,
+        FormInlineView,
+        PaginationView,
+        FormWithFilesView,
+        DefaultFormView,
+        MiscView,
+        DefaultFormsetView,
+        DefaultFormByFieldView,
+        )
+
+urlpatterns = [
+            path('index', views.index, name='index'),
+            url(r"^$", HomePageView.as_view(), name="home"),
+            url(r"^formset$", DefaultFormsetView.as_view(), name="formset_default"),
+            url(r"^form$", DefaultFormView.as_view(), name="form_default"),
+            url(r"^form_by_field$", DefaultFormByFieldView.as_view(), name="form_by_field"),
+            url(r"^form_horizontal$", FormHorizontalView.as_view(), name="form_horizontal"),
+            url(r"^form_inline$", FormInlineView.as_view(), name="form_inline"),
+            url(r"^form_with_files$", FormWithFilesView.as_view(), name="form_with_files"),
+            url(r"^pagination$", PaginationView.as_view(), name="pagination"),
+            url(r"^misc$", MiscView.as_view(), name="misc"),
+            ]
---
+++ setup.sh
@@ -0,0 +1,25 @@
+###
+#	Author:         Shibaji Chakraborty
+#	Organization:   SuperDARN VT
+#	Description:    This shell script is used to get the initial setup for the reenforced learning UI.
+###
+echo 
+echo "***********************************************************************************************************************"
+conda create -n reenforce python=3.6 anaconda
+
+echo Environment :: Web Environment
+echo Framework :: Django
+echo Intended Audience :: SuperDARN 
+echo License :: MIT License
+echo Programming Language :: Python
+echo Programming Language :: Python :: 3.6
+echo keywords :: django chart highcharts ajax class based view
+echo 
+
+conda install django
+pip install django-bootstrap3
+django-admin startproject reenforce_ui
+cd reenforce_ui 
+python manage.py startapp ui
+cd ..
+echo "***********************************************************************************************************************"
